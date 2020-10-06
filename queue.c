9c937f7d0c9d6227b9eecadaf85a3573ecbe8a9d
Holychung <mickey94378@gmail.com>
2020-10-06 16:55:10 +0800

    Remove comment

--- queue.c
+++ queue.c
@@ -24,8 +24,6 @@ queue_t *q_new()
 /* Free all storage used by queue */
 void q_free(queue_t *q)
 {
-    /* TODO: How about freeing the list elements and the strings? */
-    /* Free queue structure */
     if (!q)
         return;
 
